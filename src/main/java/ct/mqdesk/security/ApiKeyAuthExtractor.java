741e265da6875cb67a3b21325bf839441c73e661
Achille <achille.mbougueng@chillo.tech>
2024-02-06 09:33:04 +0100

    Validation formulaire

--- src/main/java/ct/mqdesk/security/ApiKeyAuthExtractor.java
+++ src/main/java/ct/mqdesk/security/ApiKeyAuthExtractor.java
@@ -3,6 +3,7 @@ package ct.mqdesk.security;
 import ct.mqdesk.entity.Token;
 import ct.mqdesk.service.TokenService;
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Component;
 import java.util.Optional;
 
 @Component
+@Slf4j
 public class ApiKeyAuthExtractor {
 
     TokenService tokenService;
@@ -24,6 +26,9 @@ public class ApiKeyAuthExtractor {
     public Optional<Authentication> extract(final HttpServletRequest request) {
         final String providedSecret = request.getHeader("X-API-SECRET");
         final String providedKey = request.getHeader("X-API-KEY");
+
+        ApiKeyAuthExtractor.log.info("Application key %s", providedKey);
+        ApiKeyAuthExtractor.log.info("Application secret %s", providedSecret);
         final Token token = this.tokenService.findByApplicationNameAndActive(providedKey, true);
 
         final boolean isValidKey = providedKey.equals(token.getApplication().getName());

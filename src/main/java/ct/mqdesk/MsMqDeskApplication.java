cd07bda2ecd3d5ad8f42ac66cdee08909e347e05
Achille <achille.mbougueng@chillo.tech>
2024-02-10 16:06:48 +0100

    deploiement

--- src/main/java/ct/mqdesk/MsMqDeskApplication.java
+++ src/main/java/ct/mqdesk/MsMqDeskApplication.java
@@ -1,8 +1,6 @@
 package ct.mqdesk;
 
-import ct.mqdesk.service.ClientApplicationService;
 import feign.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -11,22 +9,11 @@ import org.springframework.context.annotation.Bean;
 @EnableFeignClients
 @SpringBootApplication
 public class MsMqDeskApplication {
-    @Autowired
-    private ClientApplicationService clientApplicationService;
 
     public static void main(final String[] args) {
         SpringApplication.run(MsMqDeskApplication.class, args);
     }
 
-    /*
-        @Bean
-        public CommandLineRunner startup() {
-
-            return args ->
-                    this.clientApplicationService.generate("front-mqdesk");
-
-        }
-    */
     @Bean
     Logger.Level feignLoggerLevel() {
         return Logger.Level.FULL;

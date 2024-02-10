26ba237a0a2dfe2266a4e7d201b94d403c05abbd
Achille <achille.mbougueng@chillo.tech>
2024-02-10 09:07:00 +0100

    deploiement

--- src/main/java/ct/mqdesk/entity/ClientApplication.java
+++ src/main/java/ct/mqdesk/entity/ClientApplication.java
@@ -1,5 +1,6 @@
 package ct.mqdesk.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -20,5 +21,6 @@ public class ClientApplication {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @Column(unique = true)
     private String name;
 }
